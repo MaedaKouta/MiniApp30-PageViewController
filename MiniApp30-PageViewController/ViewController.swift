14b25833fb50be699089ef95de7550e785923853
MaedaKouta <maedakoutaa@gmail.com>
2022-03-16 10:16:57 +0900

    Initial Commit

---
+++ MiniApp30-PageViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp30-PageViewController
+//
+//  Created by 前田航汰 on 2022/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
